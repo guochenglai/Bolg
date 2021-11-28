0cef309814427181fd22c795824407bbd121f382
guochenglai <gcl272633743@163.com>
2021-11-28 22:37:24 +0800

    about page

--- shell/blog-admin.sh
+++ shell/blog-admin.sh
@@ -1,2 +1,2 @@
 scp ~/Workspace/Bolg/blog-web/target/blog-admin.jar centos01@20.210.96.76:/home/centos01/workspace
-nohup java -server -Xms256m -Xmx512m -jar blog-admin.jar --spring.profiles.active=prod > nohup.out  2>&1 &
+nohup java -server -Xms256m -Xmx512m -jar blog-admin.jar  > nohup.out  2>&1 &
